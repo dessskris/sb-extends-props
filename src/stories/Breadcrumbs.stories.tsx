18795a8fb32aeb1e6e784fcda2351a302ac111a2
Desy Kristianti <desy.kristianti@oaknorth.ai>
2021-01-07 12:20:28 +0000

    Added breadcrumbRenderer

--- src/stories/Breadcrumbs.stories.tsx
+++ src/stories/Breadcrumbs.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Meta } from '@storybook/react/types-6-0';
-import { Breadcrumbs, IBreadcrumbProps } from './Breadcrumbs';
+import { Breadcrumbs, IBreadcrumbsProps, Breadcrumb, IBreadcrumbProps } from './Breadcrumbs';
 
 export default {
   title: 'Example/Breadcrumbs',
@@ -12,5 +12,12 @@ const items: IBreadcrumbProps[] = [
   { href: '/folder/one', icon: "folder-close", text: 'Folder Two' },
   { icon: "document", text: 'Document.xls' },
 ];
+const renderCurrentBreadcrumb = (props: IBreadcrumbProps) => <Breadcrumb {...props} />;
 
-export const Default = () => <Breadcrumbs items={items} collapseFrom='start' />;
+export const Default = (args: IBreadcrumbsProps) => <Breadcrumbs {...args} />;
+
+Default.args = {
+  items: items,
+  collapseFrom: 'start',
+  breadcrumbRenderer: renderCurrentBreadcrumb,
+};
